659c46ba0db70af58b008726020408b4510a043c
demonick82 <demonick82@gmail.com>
2022-03-15 23:52:38 +0300

    0. Spring test [#6881](fix)

--- src/test/java/ru/job4j/forum/RegControlTest.java
+++ src/test/java/ru/job4j/forum/RegControlTest.java
@@ -10,9 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest (classes = ForumApplication.class)
+@WebAppConfiguration
 @AutoConfigureMockMvc
 public class RegControlTest {
     @Autowired
