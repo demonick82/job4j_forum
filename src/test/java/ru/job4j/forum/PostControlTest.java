ad52db8820ee392cf86c5c04d8b046c3ee714149
demonick82 <demonick82@gmail.com>
2022-03-15 23:36:36 +0300

    0. Spring test [#6881]

---
+++ src/test/java/ru/job4j/forum/PostControlTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.forum;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest (classes = ForumApplication.class)
+@AutoConfigureMockMvc
+public class PostControlTest {
+
+    @Autowired
+    private  MockMvc mockMvc;
+
+    @Test
+    @WithMockUser
+    public void shouldReturnCreateMessage() throws Exception {
+        this.mockMvc.perform(get("/create"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("/create"));
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldReturnMyPostsMessage() throws Exception {
+        this.mockMvc.perform(get("/myPosts"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("/myPosts"));
+    }
+}
---
+++ src/test/java/ru/job4j/forum/RegControlTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.forum;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest (classes = ForumApplication.class)
+@AutoConfigureMockMvc
+public class RegControlTest {
+    @Autowired
+    private  MockMvc mockMvc;
+
+    @Test
+    @WithMockUser
+    public void shouldReturnRegMessage() throws Exception {
+        this.mockMvc.perform(get("/reg"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("/reg"));
+    }
+}
