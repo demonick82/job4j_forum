d11b0835c8a4fd9fcfa2f36091f2d23c11060d7e
demonick82 <demonick82@gmail.com>
2022-03-17 02:06:52 +0300

    0. Spring Liquibase [#302330 #276301]

--- src/main/java/ru/job4j/forum/ForumApplication.java
+++ src/main/java/ru/job4j/forum/ForumApplication.java
@@ -1,11 +1,24 @@
 package ru.job4j.forum;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class ForumApplication {
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource ds) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+        liquibase.setDataSource(ds);
+        return liquibase;
+    }
     public static void main(String[] args) {
         SpringApplication.run(ForumApplication.class, args);
     }
+
 }
